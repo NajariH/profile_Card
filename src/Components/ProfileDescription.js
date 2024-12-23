edea259f840655a8f0c18769113269bc25d5c4d1
haithem <cybelrain@hotmail.fr>
2024-12-23 19:31:41 +0100

    modification haihem

--- src/Components/ProfileDescription.js
+++ src/Components/ProfileDescription.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const ProfileDescription = () => {
-    return 
-    (
+    return (
    <div>
      <h1>Développeuse Web avec 4 ans d'expérience.</h1>
      <p>1ere expérience:</p>
+     <h2>modification haithem</h2>
      <p>2emme experience :</p>
      <p>3emme expérience :</p>
    </div>
@@ -13,4 +13,4 @@ const ProfileDescription = () => {
 )};
 
 export default ProfileDescription;
-//ici j'ai rien changé 
+//pas d'affichage oui
